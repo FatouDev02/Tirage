9e7357f94c2e09e8b758323b649e001cc441828b
fccoulibaly <fatoumatacoulibaly4155@gmail.com>
2022-08-19 11:18:01 +0000

    postulant&appproperties

--- src/main/java/com/example/ApiTirage/Services/Impl/ListeServiceImpl.java
+++ src/main/java/com/example/ApiTirage/Services/Impl/ListeServiceImpl.java
@@ -1,4 +1,7 @@
 package com.example.ApiTirage.Services.Impl;
 
+
 public class ListeServiceImpl {
+
+
 }
--- src/main/java/com/example/ApiTirage/Services/PostulantServices.java
+++ src/main/java/com/example/ApiTirage/Services/PostulantServices.java
@@ -1,4 +1,13 @@
 package com.example.ApiTirage.Services;
 
+import com.example.ApiTirage.Models.Postulants;
+
+import java.util.List;
+
 public interface PostulantServices {
+    Postulants Ajout(Postulants pays);
+    List<Postulants> liste();
+    Postulants Modifier(Postulants pays,Integer Id_postulant);
+    String SupprimerbyId(Integer Id_postulant);
+
 }
