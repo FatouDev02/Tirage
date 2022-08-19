962e47f94cb4c2f103b191930daf373ec2f0e34c
fccoulibaly <fatoumatacoulibaly4155@gmail.com>
2022-08-19 11:18:01 +0000

    postulant&appproperties

--- src/main/java/com/example/ApiTirage/Models/Postulants.java
+++ src/main/java/com/example/ApiTirage/Models/Postulants.java
@@ -1,9 +1,20 @@
 package com.example.ApiTirage.Models;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Data
+@Entity
 public class Postulants {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id_postulant;
     private String nom;
     private String prenom;
-    private Long Id_postulants;
-
+    private String email;
+    private Integer numero;
 }
