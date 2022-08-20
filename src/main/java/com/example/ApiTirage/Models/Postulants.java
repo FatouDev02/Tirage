b370e980f5a350ff7830d45043c34ea5589c418e
aamaiga <maigaabdoulaziz795@gmail.com>
2022-08-20 13:34:38 +0000

    Trie

--- src/main/java/com/example/ApiTirage/Models/Postulants.java
+++ src/main/java/com/example/ApiTirage/Models/Postulants.java
@@ -2,29 +2,19 @@ package com.example.ApiTirage.Models;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.util.List;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 @Data
 @Entity
 public class Postulants {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer Id_postulant;
+    private Long Id_postulant;
     private String nom;
     private String prenom;
     private String email;
-    private String numero;
-
-    @ManyToOne
-    private ListImport listImport;
-
-    @ManyToMany
-    @JoinTable(
-            name = "Postulants_Tirage",
-            joinColumns = @JoinColumn(name="Postulants_id"),
-            inverseJoinColumns = @JoinColumn(name = "Tirage_id")
-    )
-    private List<Tirage>  tirage;
+    private Integer numero;
 }
